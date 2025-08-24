15d1ffcc88fa5c9394980e046dd54942358f3629
unknown <anthony@anthony.blog.br>
2025-08-24 12:18:43 -0300

    Add arquivos no repo

---
+++ Cadastro_Pessoal.java
@@ -0,0 +1,17 @@
+package cadastro_pessoal;
+
+
+public class Cadastro_Pessoal {
+
+    
+    public static void main(String[] args) {
+    
+        Estudante estudante = new Estudante("Andre", 21, "Andre_Example@gmail.com");
+        System.out.println("Ola Sr. " + estudante.getNome() + estudante.getEmail());
+    
+    
+    
+    
+    }
+    
+}
